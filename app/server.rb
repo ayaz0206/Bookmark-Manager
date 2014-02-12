c9192e12ae7db2b3e591b7c0044bc4565b79f247
Ayazg <ayaz0206@gmail.com>
2014-02-12 18:10:25 +0000

    Moved models to app

--- app/server.rb
+++ app/server.rb
@@ -3,9 +3,9 @@ require 'data_mapper'
 require 'rack-flash'
 require 'sinatra/partial'
 
-require './lib/link'
-require './lib/tag'
-require './lib/user'
+require_relative 'models/link'
+require_relative 'models/tag'
+require_relative 'models/user'
 
 require_relative 'helpers/application'
 require_relative 'data_mapper_setup'
