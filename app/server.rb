0d1b3d151a33d50324b61d72bfad4e5a7ac0607e
Ayazg <ayaz0206@gmail.com>
2014-02-12 12:22:17 +0000

    Rendering the users and Flash message if password is wrong

--- app/server.rb
+++ app/server.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'data_mapper'
+require 'rack-flash'
 
 require './lib/link'
 require './lib/tag'
@@ -10,6 +11,7 @@ require_relative 'data_mapper_setup'
 
 enable :sessions
 set :session_secret, 'super secret'
+use Rack::Flash 
 
 get '/' do
 	@links = Link.all
@@ -32,13 +34,19 @@ get '/tags/:text' do
 end
 
 get '/users/new' do
+	@user = User.new
 	erb :"users/new"
 end
 
 post '/users' do
-  user = User.create(:email => params[:email], 
+  @user = User.create(:email => params[:email], 
               :password => params[:password],
-              :password_confirmation => params[:password_confirmation])  
-  session[:user_id] = user.id
-  redirect to('/')
+              :password_confirmation => params[:password_confirmation])
+	 if @user.save               
+	  session[:user_id] = @user.id
+	  redirect to('/')
+	else
+		flash[:notice] = "Sorry, your passwords don't match!"
+		erb :"users/new"
+	end
 end
